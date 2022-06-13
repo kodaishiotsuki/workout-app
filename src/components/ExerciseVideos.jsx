f14280e0521b12667c1f947b2d4f3394a4354c98
kodaishiotsuki <0711dr5@gmail.com>
2022-06-13 19:08:49 +0900

    exersice 詳細データ取得

---
+++ src/components/ExerciseVideos.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ExerciseVideos() {
+  return (
+    <div>ExerciseVideos</div>
+  )
+}
--- src/pages/ExerciseDetail.jsx
+++ src/pages/ExerciseDetail.jsx
@@ -1,7 +1,39 @@
-import React from 'react'
+import { Box } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Detail from "../components/Detail";
+import ExerciseVideos from "../components/ExerciseVideos";
+import SimilarExercises from "../components/SimilarExercises";
+
+import { exerciseOptions, fetchData } from "../utils/fetchData";
 
 export default function ExerciseDetail() {
+  const [exerciseDetail, setExerciseDetail] = useState({});
+
+  //動的なidなどのパラメータ値を扱う（exerciseのid）
+  const { id } = useParams();
+
+  // api呼び出し
+  useEffect(() => {
+    const fetchExerciseData = async () => {
+      const exerciseDbUrl = "https://exercisedb.p.rapidapi.com";
+      const youtubeSearchUrl =
+        "https://youtube-search-and-download.p.rapidapi.com";
+
+      const exerciseDetailData = await fetchData(
+        `${exerciseDbUrl}/exercises/${id}`,
+        exerciseOptions
+      );
+      setExerciseDetail(exerciseDetailData);
+    };
+    fetchExerciseData();
+  }, [id]);
+
   return (
-    <div>ExerciseDetail</div>
-  )
+    <Box>
+      <Detail exerciseDetail={exerciseDetail} />
+      <ExerciseVideos />
+      <SimilarExercises />
+    </Box>
+  );
 }
