8f41926be8792df77866c35874a469bbd576c02f
kodaishiotsuki <0711dr5@gmail.com>
2022-06-13 23:32:04 +0900

    fotter

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import { Box, Stack, Typography } from "@mui/material";
+import React from "react";
+
+import Logo from "../assets/images/Logo-1.png";
 
 export default function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <Box mt='80px' bgcolor='#fff3f4' >
+      <Stack gap='40px' alignItems='center' px='40px' pt='24px'>
+        <img src={Logo} alt='logo' width='200px' height='50px' />
+        <Typography variant='h5' pb='40px' mt='40px'>
+          Made with 🍺 by JavaScript Bigger
+        </Typography>
+      </Stack>
+    </Box>
+  );
 }
