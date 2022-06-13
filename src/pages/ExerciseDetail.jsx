93750e3e7e4415a2314230676831cf338625bbcc
kodaishiotsuki <0711dr5@gmail.com>
2022-06-13 19:36:31 +0900

    Detail component

--- src/pages/ExerciseDetail.jsx
+++ src/pages/ExerciseDetail.jsx
@@ -21,7 +21,7 @@ export default function ExerciseDetail() {
         "https://youtube-search-and-download.p.rapidapi.com";
 
       const exerciseDetailData = await fetchData(
-        `${exerciseDbUrl}/exercises/${id}`,
+        `${exerciseDbUrl}/exercises/exercise/${id}`,
         exerciseOptions
       );
       setExerciseDetail(exerciseDetailData);
